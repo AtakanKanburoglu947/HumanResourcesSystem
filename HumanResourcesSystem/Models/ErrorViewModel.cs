b3e73ae693cc1e28fe96eaf02a95948efac4a835
Atakan Kanburoglu <88630930+AtakanKanburoglu947@users.noreply.github.com>
2025-02-25 22:24:03 +0300

    Add project files.

---
+++ HumanResourcesSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HumanResourcesSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
